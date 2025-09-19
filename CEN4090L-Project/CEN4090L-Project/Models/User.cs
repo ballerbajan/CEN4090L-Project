061eb8347ef2fe57e5d8b6a19e79c2d1645f2ac8
Isiaq Allison <ballerbajan@gmail.com>
2025-09-19 09:31:00 -0400

    Renamed preexisting files to follow MVVM framework and ADDED User.cs file under models #2

---
+++ CEN4090L-Project/CEN4090L-Project/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEN4090L_Project.Models
+{
+    internal class User
+    {
+    }
+}
