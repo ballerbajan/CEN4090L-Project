258e0d8689dacd7adac81a31b479032b9a70e18d
Atalia Hernandez <ah22bd@fsu.edu>
2025-10-03 19:40:29 -0400

    adding a rough functionality for budget model

--- CEN4090L-Project/CEN4090L-Project/Models/Budget.cs
+++ CEN4090L-Project/CEN4090L-Project/Models/Budget.cs
@@ -2,7 +2,10 @@ using System;
 
 public class Budget
 {
-	public Budget() { }
+	//list of expense managed by the budget
+    private List<Expense> expenses = new List<Expense>();
+
+    public Budget() { }
 
 	public string? Title { get; set; }
 
@@ -10,5 +13,20 @@ public class Budget
 
 	public int? Expeneses { get; set; }
 
-	public int? totalExpenses { get; set; }
+	//calculates the total expense per expenses
+	public int? totalExpense { }
+
+	//returns a list with the specified category
+	public list<Expense> returnCategory (int id)
+	{
+	    //goes through the ID and returns list	
+	}
+
+	//remove desire expense from the list of expenses
+	public void removeExpense(Expense e) { }
+
+    //add desire expense from the list of expenses
+    public void addExpense(Expense e) { }
+
+
 }
