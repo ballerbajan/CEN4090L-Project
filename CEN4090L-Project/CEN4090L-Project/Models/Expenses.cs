f7203f88d7fe7c0edf776b50e4acf065a42dde37
Gcooper4 <gcc0163@gmail.com>
2025-10-06 11:08:09 -0400

    Create Expenses model

---
+++ CEN4090L-Project/CEN4090L-Project/Models/Expenses.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Expenses
+{
+	public Expenses() {}
+
+	public string? Title { get; set; }
+
+	public decimal? Amount { get; set; }
+
+	public override string ToString()
+	{
+		return $"{Title}: {Amount}";
+    }
+}
