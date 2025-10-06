b1a667345001b4c81579245bdb1bcfe71b72e628
gcc01 <gcc01@LAPTOP-H70690DL>
2025-10-06 11:15:23 -0400

    adding Expenses class

---
+++ CEN4090L-Project/CEN4090L-Project/Models/Expenses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Expenses
+{
+    public Expenses() { }
+
+    public string? Title { get; set; }
+
+    public decimal? Amount { get; set; }
+
+    public string? Category { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Title}: {Amount}";
+    }
+}
