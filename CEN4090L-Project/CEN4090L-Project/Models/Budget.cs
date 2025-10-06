542c9540689eaa7929b2711fc8b4475453513ede
Atalia Hernandez <ah22bd@fsu.edu>
2025-10-06 12:16:53 -0400

    budget updated

--- CEN4090L-Project/CEN4090L-Project/Models/Budget.cs
+++ CEN4090L-Project/CEN4090L-Project/Models/Budget.cs
@@ -1,36 +1,65 @@
 using System;
-using System.Collections.Generic;
 
 public class Budget
 {
-	//list of expense managed by the budget
-    private List<Expenses> expenses = new List<Expenses>();
+    //list of expense managed by the budget
+    private List<Expense> expenses = new List<Expense>();
 
     public Budget() { }
 
-	public string? Title { get; set; }
+    public string? Title { get; set; }
 
-	public int? Priority { get; set; }
+    public int? Priority { get; set; }
 
-	public int? Expeneses { get; set; }
+    public list<Expense> Expeneses { get; set; }
 
-	//calculates the total expense per expenses
-	public int? totalExpense {
-		get; set;
+    //calculates the total expense per expenses
+    public decimal? totalExpense
+    {
+
+        decimal? totalExpenses = 0;
+	    foreach (e in Expenses)
+		   totalExpenses += e.amount;
+		return totalExpenses;
 	}
 
-	//returns a list with the specified category
-	public List<Expenses>? returnCategory (int id)
-	{
-	    //goes through the ID and returns list	
-		throw new NotImplementedException();
+//returns a list with the specified category
+public list<Expense> returnCategory(int id)
+{
+    if (id < 0 && id > 3)
+        return null;
+    list<Expense> category = new List<Expense>();
+
+    foreach (e in Expenses)
+        if (e.id == id)
+            category.add(e);
+
+    return category;
+
+
+}
+
+//remove desire expense from the list of expenses
+public void removeExpense(Expense e1)
+{
+    bool removed = 0;
+    foreach (e in Expenses)
+    {
+        //checks if the expense exiist in the list
+        if (e.title == e1.title)
+        {
+            removed = Expenses.remove(e1);
+
+        }
     }
+    return removed;
+}
 
-	//remove desire expense from the list of expenses
-	public void removeExpense(Expenses e) { }
-
-    //add desire expense from the list of expenses
-    public void addExpense(Expenses e) { }
+//add desire expense from the list of expenses
+public void addExpense(Expense e)
+{
+    Expenses.add(e);
+}
 
 
 }
