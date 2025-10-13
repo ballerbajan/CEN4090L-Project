bdb4570b392c7d2f892a455a4cbc9a66d59f84fe
Kate Payen <139496388+kpayen@users.noreply.github.com>
2025-10-13 11:07:05 -0400

    Update Group.cs

--- CEN4090L-Project/CEN4090L-Project/Models/Group.cs
+++ CEN4090L-Project/CEN4090L-Project/Models/Group.cs
@@ -1,6 +1,4 @@
 // Group.cs
-using System.Collections.Generic;
-
 namespace BudgetApp
 {
     public class Group
@@ -9,42 +7,35 @@ namespace BudgetApp
         public int ID { get; set; }
         public string Name { get; set; }
 
-        // Private backing field for list of users
-        private List<string> _users = new List<string>();
-        public List<string> Users
+        // Private backing field for user
+        private string _user;
+        public string User
         {
-            get { return _users; }
-            set { _users = value ?? new List<string>(); }
+            get { return _user; }
+            set { _user = value; }
         }
 
-        // List of savings goals
-        public List<string> SavingsGoals { get; set; } = new List<string>();
+        // Private backing field for savings goal
+        private string _savingsGoal;
+        public string SavingsGoal
+        {
+            get { return _savingsGoal; }
+            set { _savingsGoal = value; }
+        }
 
         // Budget
         public decimal Budget { get; set; }
 
-        // Constructor
-        public Group(int id, string name, decimal budget)
+        // Constructors
+        public Group() { }
+
+        public Group(int id, string name, string user, string savingsGoal, decimal budget)
         {
             ID = id;
             Name = name;
+            User = user;
+            SavingsGoal = savingsGoal;
             Budget = budget;
-        }
-
-        // Default constructor
-        public Group() { }
-
-        // Example methods
-        public void AddUser(string user)
-        {
-            if (!string.IsNullOrWhiteSpace(user) && !_users.Contains(user))
-                _users.Add(user);
-        }
-
-        public void AddSavingsGoal(string goal)
-        {
-            if (!string.IsNullOrWhiteSpace(goal))
-                SavingsGoals.Add(goal);
         }
     }
 }
