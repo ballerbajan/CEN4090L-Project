e2e4c117a742426445937a814f9a6c2d009746d3
Isiaq Allison <ballerbajan@gmail.com>
2025-10-23 17:51:56 -0400

    init commit for #19

---
+++ CEN4090L-Project/CEN4090L-Project/ViewModels/ExpenseDetailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CEN4090L_Project.Models;
+using CEN4090L_Project.Services;
+namespace CEN4090L_Project.ViewModels
+{
+    public class ExpensePageViewModel
+    {
+        private Expense Model { get; set; }
+        public ExpensePageViewModel()
+        {
+            Model = new Expense();
+        }
+
+        public void AddOrUpdateExpense()
+        {
+            TransactionServiceProxy.Current.addExpense(Model);
+        }
+    }
+}
