b018e4c67bfac570dab888e935e77faea0bf51e6
Isiaq Allison <ballerbajan@gmail.com>
2025-11-07 16:48:13 -0500

    Update Expense.cs
    Removed duplicate

--- CEN4090L-Project/CEN4090L-Project/Models/Expense.cs
+++ CEN4090L-Project/CEN4090L-Project/Models/Expense.cs
@@ -6,9 +6,6 @@ public class Expense
 	public Expense() {}
 	public int Id { get; set; }
 	public string? Title { get; set; }
-
-	public int? Id { get; set; }
-
 	public decimal? Amount { get; set; }
 
     public string? Description { get; set; }
