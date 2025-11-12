dc5280628ed0e8f0043b3265384bac4d4fbedd39
Kate Payen <139496388+kpayen@users.noreply.github.com>
2025-11-11 22:35:17 -0500

    Change BindingContext to DashboardViewModel.Instance
    Updated BindingContext to use singleton instance of DashboardViewModel.

--- CEN4090L-Project/CEN4090L-Project/Views/MainDashboardPage.xaml.cs
+++ CEN4090L-Project/CEN4090L-Project/Views/MainDashboardPage.xaml.cs
@@ -7,6 +7,18 @@ public partial class MainDashboardPage : ContentPage
     public MainDashboardPage()
     {
         InitializeComponent();
-        BindingContext = new DashboardViewModel();
+        //BindingContext = new DashboardViewModel();
+        BindingContext = DashboardViewModel.Instance;
     }
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        // Force refresh when returning to page
+        if (BindingContext is DashboardViewModel vm)
+        {
+            vm.OnPropertyChanged(nameof(vm.RecentExpenses));
+        }
+    }
+
 }
