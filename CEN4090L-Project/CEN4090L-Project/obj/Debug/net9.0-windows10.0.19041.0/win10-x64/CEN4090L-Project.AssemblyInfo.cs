d835dbba6103ca2901133ef2c567fdbc2aa43bbb
Isiaq Allison <ballerbajan@gmail.com>
2025-12-09 16:45:42 -0500

    Revert "CollegeCompanion API init"
    This reverts commit 9f174304ff9f708f2ab930330bdcdabbba5818ad.

--- CEN4090L-Project/CEN4090L-Project/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/CEN4090L-Project.AssemblyInfo.cs
+++ CEN4090L-Project/CEN4090L-Project/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/CEN4090L-Project.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CEN4090L-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9cb12f0736204b0a6da88b6bd549f4cc4004c5bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+88aaed8822afc55fabcaf87c3619ca34805b93f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CEN4090L-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CEN4090L-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
